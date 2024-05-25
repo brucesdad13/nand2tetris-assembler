c097d8f392f716d73b7c88da74604d70f5f16ad7
brucesdad13 <brucesdad13@gmail.com>
2024-05-25 07:01:28 -0400

    Cleanup as recommended by IDE

--- src/Code.java
+++ src/Code.java
@@ -26,25 +26,23 @@ public class Code {
      * @return String the destination binary code
      */
     public String dest(String mnemonic) {
-        switch (mnemonic) {
-            case "M": // memory register
-                return "001";
-            case "D": // data register
-                return "010";
-            case "MD": // memory and data register
-                return "011";
-            case "A": // address register
-                return "100";
-            case "AM": // address and memory register
-                return "101";
-            case "AD": // address and data register
-                return "110";
-            case "AMD": // address, data, and memory register
-                return "111";
-            case null:
-            default:
-                return "000";
-        }
+        return switch (mnemonic) {
+            case "M" -> // memory register
+                    "001";
+            case "D" -> // data register
+                    "010";
+            case "MD" -> // memory and data register
+                    "011";
+            case "A" -> // address register
+                    "100";
+            case "AM" -> // address and memory register
+                    "101";
+            case "AD" -> // address and data register
+                    "110";
+            case "AMD" -> // address, data, and memory register
+                    "111";
+            case null, default -> "000";
+        };
     }
 
     /**
@@ -78,67 +76,65 @@ public class Code {
      * @return String the computation binary code
      */
     public String comp(String mnemonic) {
-        switch (mnemonic) {
-            case "0": // zero
-                return "0101010";
-            case "1": // one
-                return "0111111";
-            case "-1": // negative one
-                return "0111010";
-            case "D": // D register
-                return "0001100";
-            case "A": // A register
-                return "0110000";
-            case "!D": // not D (complement)
-                return "0001101";
-            case "!A": // not A (complement)
-                return "0110001";
-            case "-D": // negate D
-                return "0001111";
-            case "-A": // negate A
-                return "0110011";
-            case "D+1": // D plus 1 (increment D)
-                return "0011111";
-            case "A+1": // A plus 1 (increment A)
-                return "0110111";
-            case "D-1": // D minus 1 (decrement D)
-                return "0001110";
-            case "A-1": // A minus 1 (decrement A)
-                return "0110010";
-            case "D+A": // D plus A
-                return "0000010";
-            case "D-A": // D minus A
-                return "0010011";
-            case "A-D": // A minus D
-                return "0000111";
-            case "D&A": // D AND A (bitwise AND)
-                return "0000000";
-            case "D|A": // D OR A (bitwise OR)
-                return "0010101";
-            case "M": // M register
-                return "1110000";
-            case "!M": // not M (complement)
-                return "1110001";
-            case "-M": // negate M
-                return "1110011";
-            case "M+1": // M plus 1 (increment M)
-                return "1110111";
-            case "M-1": // M minus 1 (decrement M)
-                return "1110010";
-            case "D+M": // D plus M
-                return "1000010";
-            case "D-M": // D minus M
-                return "1010011";
-            case "M-D": // M minus D
-                return "1000111";
-            case "D&M": // D AND M (bitwise AND)
-                return "1000000";
-            case "D|M": // D OR M (bitwise OR)
-                return "1010101";
-            case null: // no computation
-            default:
-                return "0000000";
-        }
+        return switch (mnemonic) {
+            case "0" -> // zero
+                    "0101010";
+            case "1" -> // one
+                    "0111111";
+            case "-1" -> // negative one
+                    "0111010";
+            case "D" -> // D register
+                    "0001100";
+            case "A" -> // A register
+                    "0110000";
+            case "!D" -> // not D (complement)
+                    "0001101";
+            case "!A" -> // not A (complement)
+                    "0110001";
+            case "-D" -> // negate D
+                    "0001111";
+            case "-A" -> // negate A
+                    "0110011";
+            case "D+1" -> // D plus 1 (increment D)
+                    "0011111";
+            case "A+1" -> // A plus 1 (increment A)
+                    "0110111";
+            case "D-1" -> // D minus 1 (decrement D)
+                    "0001110";
+            case "A-1" -> // A minus 1 (decrement A)
+                    "0110010";
+            case "D+A" -> // D plus A
+                    "0000010";
+            case "D-A" -> // D minus A
+                    "0010011";
+            case "A-D" -> // A minus D
+                    "0000111";
+            case "D&A" -> // D AND A (bitwise AND)
+                    "0000000";
+            case "D|A" -> // D OR A (bitwise OR)
+                    "0010101";
+            case "M" -> // M register
+                    "1110000";
+            case "!M" -> // not M (complement)
+                    "1110001";
+            case "-M" -> // negate M
+                    "1110011";
+            case "M+1" -> // M plus 1 (increment M)
+                    "1110111";
+            case "M-1" -> // M minus 1 (decrement M)
+                    "1110010";
+            case "D+M" -> // D plus M
+                    "1000010";
+            case "D-M" -> // D minus M
+                    "1010011";
+            case "M-D" -> // M minus D
+                    "1000111";
+            case "D&M" -> // D AND M (bitwise AND)
+                    "1000000";
+            case "D|M" -> // D OR M (bitwise OR)
+                    "1010101"; // no computation
+            case null, default -> "0000000";
+        };
     }
 
     /**
@@ -158,24 +154,22 @@ public class Code {
      * @return String the jump binary code
      */
     public String jump(String mnemonic) {
-        switch (mnemonic) {
-            case "JGT": // if out > 0 jump
-                return "001";
-            case "JEQ": // if out == 0 jump
-                return "010";
-            case "JGE": // if out >= 0 jump
-                return "011";
-            case "JLT": // if out < 0 jump
-                return "100";
-            case "JNE": // if out != 0 jump
-                return "101";
-            case "JLE": // if out <= 0 jump
-                return "110";
-            case "JMP": // unconditional jump
-                return "111";
-            case null: // no jump
-            default:
-                return "000";
-        }
+        return switch (mnemonic) {
+            case "JGT" -> // if out > 0 jump
+                    "001";
+            case "JEQ" -> // if out == 0 jump
+                    "010";
+            case "JGE" -> // if out >= 0 jump
+                    "011";
+            case "JLT" -> // if out < 0 jump
+                    "100";
+            case "JNE" -> // if out != 0 jump
+                    "101";
+            case "JLE" -> // if out <= 0 jump
+                    "110";
+            case "JMP" -> // unconditional jump
+                    "111"; // no jump
+            case null, default -> "000";
+        };
     }
 }
